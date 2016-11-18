b390e3580c93334e7630e7728ab0fb812bea32e0
Őri László <ori.laszlo@virgo.hu>
2016-11-18 09:38:55 +0100

    Initial Commit

---
+++ VMBase/VMBase.h
@@ -0,0 +1,19 @@
+//
+//  VMBase.h
+//  VMBase
+//
+//  Created by Őri László on 2016. 11. 18..
+//  Copyright © 2016. VirgoMobile Kft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VMBase.
+FOUNDATION_EXPORT double VMBaseVersionNumber;
+
+//! Project version string for VMBase.
+FOUNDATION_EXPORT const unsigned char VMBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VMBase/PublicHeader.h>
+
+
